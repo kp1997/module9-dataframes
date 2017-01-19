f4f8c60c47b3c0b863026189fc8da8af4372b3f9
Kyle Porter <kyle.porter1997@gmail.com>
2017-01-19 15:20:03 -0800

    Add solutions

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,34 +1,35 @@
 # Load R's "USPersonalExpenditure" dataset using the "data()" function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data(USPersonalExpenditure)
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable as an argument
 # to the `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+le.data <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(le.data)
 
 # Why are they so strange? Think about whether you could use a number like 1940
 # with dollar notation!
 
-# What are the row names of your dataframe?
 
+# What are the row names of your dataframe?
+row.names(le.data)
 
 # Create a column "category" that is equal to your rownames
-
+le.data$category <- rownames(le.data)
 
 # How much money was spent on personal care in 1940?
-
+le.data["Personal Care", "X1940"]
 
 # How much money was spent on Food and Tobacco in 1960?
-
+le.data["Food and Tobacco", "X1960"]
 
 # What was the highest expenditure category in 1960?
 # Hint: use the `max()` function to find the largest, then compare that to the column
